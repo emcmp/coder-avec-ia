530cfb3d67c3acc0639294576dabe1a4073451e5
emcmp <130682343+emcmp@users.noreply.github.com>
2026-07-21 23:32:30 -0400

    fix(site): load Microsoft link transformer as remark plugin

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -4,7 +4,7 @@ import { themes } from "prism-react-renderer";
 const siteConfig = require("./config");
 const lightCodeTheme = themes.vsLight;
 const darkCodeTheme = themes.vsDark;
-const absoluteMicrosoftLinks = require.resolve("./plugins/absolute-microsoft-links");
+const absoluteMicrosoftLinks = require("./plugins/absolute-microsoft-links");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
